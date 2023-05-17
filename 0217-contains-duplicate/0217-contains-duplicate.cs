056207cf2745db3341dcbcc1238f9991d60bf01a
Raouf Alaadin <107007606+RaoufAlaadin@users.noreply.github.com>
2023-05-17 14:57:39 +0300

    Time: 198 ms (80.35%), Space: 55.5 MB (13.89%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cs
+++ 0217-contains-duplicate/0217-contains-duplicate.cs
@@ -1,31 +1,43 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
+        
+      
+        
+        
+        var DuplicateTrackerSet = new HashSet<int>(); 
+        
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if (DuplicateTrackerSet.Contains(nums[i]))
+            {
+                return true; 
+            }
+            else 
+                DuplicateTrackerSet.Add(nums[i]);
+        }
+        
+        return false; 
+        
+    }
+    
+    
+}
 
-            /* Time comlixity: O(n)
+
+/// Draft 
+
+/*
+            -Time comlixity: O(n)
                 As we fill the hash set using the same for loop we are checking by
                 and accessing those elements using contains() is O(1) as hashing made us 
                 know the exact spot of the element
                 
-                Space complixty: O(n)
+            -Space complixty: O(n)
                 
                 Took more space due to the hashing which declares a new element every iteration
-                   */
-        HashSet<int> hashCheck = new HashSet<int>();
-
-        for ( int i = 0; i < nums.Length; i++)
-        {
-                 if (hashCheck.Contains(nums[i])) 
-                         return true;
-                else
-                    hashCheck.Add(nums[i]);
-        }
-        
-
-        return false; 
-        
-    }
+                   
 
 
-    // https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.hashset-1?view=net-7.0
-} 
+         // https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.hashset-1?view=net-7.0
 
+*/
