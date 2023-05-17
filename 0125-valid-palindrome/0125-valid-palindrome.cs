31df0a2dc11b09c5d45d4fad74fb5285cf863843
Raouf Alaadin <107007606+RaoufAlaadin@users.noreply.github.com>
2023-05-18 00:32:20 +0300

    Time: 95 ms (28.83%), Space: 40.7 MB (60.56%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cs
+++ 0125-valid-palindrome/0125-valid-palindrome.cs
@@ -3,36 +3,41 @@ public class Solution {
         
         
         // Alphanumeric ===> Alphabet or numbers 
+        
+        // We built our own IsAlphaNumeric() function
+        // Instead of using IsDigit() and IsLetter() 
     
-        string GoodString = ""; 
-        
-        if ( s == "")
-            return true; 
-        
-        string str = s.ToLower(); 
-        
-        for(int i = 0; i < str.Length; i++)
-        {
-            if (Char.IsLetterOrDigit(str[i]))
-                GoodString += str[i];
-        }
-        
-        
         var StartIndex = 0; 
-        var EndIndex = GoodString.Length -1 ; 
+        var EndIndex = s.Length -1 ; 
+        
+        s = s.ToLower();  // --> Everything to lowerCase. 
         
        while( StartIndex < EndIndex)
        {
-           if(GoodString[StartIndex] != GoodString[EndIndex])
-                       return false; 
-           
-                    StartIndex++;
-                    EndIndex--;
+               if (!IsAlphaNumeric(s[StartIndex]))
+                   StartIndex++; 
 
+               else if (!IsAlphaNumeric(s[EndIndex]))
+                   EndIndex--; 
+
+               else{
+                        if (s[StartIndex] != s[EndIndex])
+                            return false; 
+
+                        StartIndex++;
+                        EndIndex--;
+               }
+         
         }
         
         return true; 
 
+    }
+    
+    
+    public bool IsAlphaNumeric(char x)
+    {
+        return (x >= 'A' && x <= 'Z') || (x >= 'a' && x <= 'z')  || (x >= '0' && x <= '9');
     }
 }
 
@@ -43,9 +48,53 @@ public class Solution {
 /// Draft 
 
 
+// Note: 
+
 /*
         1- string to lower case 
         2- remove all non - alphanumeric characters
         
         
 */
+
+
+
+// Method-1 ===> BAD, it uses a new string, which uses tons of memory. 
+
+// public class Solution {
+//     public bool IsPalindrome(string s) {
+        
+        
+//         // Alphanumeric ===> Alphabet or numbers 
+    
+//         string GoodString = ""; 
+        
+//         if ( s == "")
+//             return true; 
+        
+//         string str = s.ToLower(); 
+        
+//         for(int i = 0; i < str.Length; i++)
+//         {
+//             if (Char.IsLetterOrDigit(str[i]))
+//                 GoodString += str[i];
+//         }
+        
+        
+//         var StartIndex = 0; 
+//         var EndIndex = GoodString.Length -1 ; 
+        
+//        while( StartIndex < EndIndex)
+//        {
+//            if(GoodString[StartIndex] != GoodString[EndIndex])
+//                        return false; 
+           
+//                     StartIndex++;
+//                     EndIndex--;
+
+//         }
+        
+//         return true; 
+
+//     }
+// }
