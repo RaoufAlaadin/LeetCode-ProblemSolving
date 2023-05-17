117654bc0f01c1f8713688f64ccf179ad0084913
Raouf Alaadin <107007606+RaoufAlaadin@users.noreply.github.com>
2023-05-17 16:59:15 +0300

    Time: 84 ms (56.33%), Space: 39.9 MB (53.86%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cs
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -2,8 +2,9 @@ public class Solution {
     public bool IsAnagram(string s, string t) {
         
         
-        var DictRight = new Dictionary<char,int>();
-        var DictLeft = new Dictionary<char,int>();
+        // number of letters in Alphabate
+        // Arrays and the use of Alphabate ASCII code ---> (VeryImportant, Used alot) 
+        int[] CountOccuranceArray = new int[26]; 
 
         
         if (s.Length != t.Length)
@@ -11,26 +12,14 @@ public class Solution {
         
         for (int i = 0; i < s.Length; i++)
         {
-            if(DictRight.ContainsKey(s[i]))
-                DictRight[s[i]]++; 
-            else
-                DictRight[s[i]] = 1; 
-            
-            if(DictLeft.ContainsKey(t[i]))
-                DictLeft[t[i]]++; 
-            else
-                DictLeft[t[i]] = 1; 
-                
-                
+                CountOccuranceArray[s[i] - 'a']++; 
+                CountOccuranceArray[t[i] - 'a']--; 
         }
         
         
-        foreach(KeyValuePair<char,int> item in DictRight)
+        for( int i = 0; i < CountOccuranceArray.Length; i++)
         {
-            if(!DictLeft.ContainsKey(item.Key))
-                return false; 
-            
-            if(DictLeft[item.Key] != item.Value)
+            if (CountOccuranceArray[i] != 0)
                 return false; 
         }
         
@@ -39,22 +28,35 @@ public class Solution {
 }
 
 
+// a Dictionary would be a hassle, Because you need to check 
+// if the key exists or not before increaseing/decreasing. 
 
 
 
 /// Draft 
 
 /*
-   -- We need a 2x dictionary,
+    
+    -- Use only one Dictionary or an Array, 
+        Keep ++ / -- based on how many occurance is found 
+        
+            ---> if the final output is 0 occurance for each `letter`
+                Then both strings equalized the occurances perfectly 
                 
+                So... it's an Anagram !! 
+                
+
+
+   -- We need a 2x dictionary,   ====> BAD !!!! 
+                 
                 key --> char 
                 value --> number of occurance 
                 
-    -- We do it for both and compare
+        - We do it for both and compare
         if a char is not inside the dict, we just return false immediatly. 
         
 
-        xxxxx Bad ==> if we Store one of the words in a HashSet. ---> No xxx bad !!! 
+    -- xxxxx Bad ==> if we Store one of the words in a HashSet. ---> No xxx bad !!! 
         
  
 
@@ -62,3 +64,48 @@ public class Solution {
         
 
 */
+
+
+
+// Method -1  ---> bad for space 
+
+
+// public class Solution {
+//     public bool IsAnagram(string s, string t) {
+        
+        
+//         var DictRight = new Dictionary<char,int>();
+//         var DictLeft = new Dictionary<char,int>();
+
+        
+//         if (s.Length != t.Length)
+//             return false; 
+        
+//         for (int i = 0; i < s.Length; i++)
+//         {
+//             if(DictRight.ContainsKey(s[i]))
+//                 DictRight[s[i]]++; 
+//             else
+//                 DictRight[s[i]] = 1; 
+            
+//             if(DictLeft.ContainsKey(t[i]))
+//                 DictLeft[t[i]]++; 
+//             else
+//                 DictLeft[t[i]] = 1; 
+                
+                
+//         }
+        
+        
+//         foreach(KeyValuePair<char,int> item in DictRight)
+//         {
+//             if(!DictLeft.ContainsKey(item.Key))
+//                 return false; 
+            
+//             if(DictLeft[item.Key] != item.Value)
+//                 return false; 
+//         }
+        
+//         return true; 
+//     }
+// }
