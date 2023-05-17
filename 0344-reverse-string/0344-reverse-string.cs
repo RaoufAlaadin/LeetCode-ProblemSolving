f92f01dd97d333a54d0582f108f26fb891b5cd68
Raouf Alaadin <107007606+RaoufAlaadin@users.noreply.github.com>
2023-05-17 14:47:35 +0300

    Time: 239 ms (90.80%), Space: 61.9 MB (21.42%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cs
+++ 0344-reverse-string/0344-reverse-string.cs
@@ -27,8 +27,13 @@ public class Solution {
 
     We would have 2 cases, 
     
-    if array length is even -> switch every char 
     
-    if it's odd --> the middle element stays in place. 
+    -- Instead of using even or odd to decide the length of the 
+        forloop. 
+        
+        ===> Just use a while-loop that breaks on a certain condition. 
+        
+    if array length is even -> switch every char        |
+    if it's odd --> the middle element stays in place.   | ===> This is bad xxxx
 
 */
