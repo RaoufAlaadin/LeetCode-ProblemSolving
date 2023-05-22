63a2da6dab022142eaa48169a573a9992f1f1102
Raouf Alaadin <107007606+RaoufAlaadin@users.noreply.github.com>
2023-05-22 13:29:11 +0300

    Time: 89 ms (50.51%), Space: 39 MB (45.39%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cs
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cs
@@ -1,28 +1,63 @@
+// public class Solution {
+//     public int FindMin(int[] nums) {
+        
+        
+       
+//         var Left = 0; 
+//         var Right = nums.Length -1 ; 
+        
+        
+       
+        
+        
+        
+//         while ( Left < Right)
+//         {
+//              //This handles 2 cases:
+//             // 1- Array has 1 element `=`
+//             // 2- Array is completely sorted.
+//             if (nums[Left] <= nums[Right])
+//                 return nums[Left];
+        
+//             var Middle = (Right - Left)/ 2  + Left ; 
+            
+//             if ( nums[Middle]  > nums[Left])
+//                     Left = Middle + 1; 
+//             else
+//                     Right = Middle;
+//         }
+        
+//         return nums[Left];
+//     }
+// }
+
+
 public class Solution {
     public int FindMin(int[] nums) {
-        
-        if(nums.Length == 1)
-            return nums[0];
-       
-        var Left = 0; 
-        var Right = nums.Length -1 ; 
-        
-        while ( Left <= Right)
-        {
-            var Middle = (Right - Left)/ 2  + Left ; 
-            
-            
-            if ( nums[Middle]  > nums[Middle + 1])
-                return nums[Middle + 1]; // --> min number
-            else if (nums[Right] < nums[Middle])
-                Left = Middle ; 
-            else if (nums[Left] > nums[Middle])
-                Right = Middle; 
-            else
-                return nums[0];
+        // Initialize left and right pointers
+        int left = 0, right = nums.Length - 1;
+
+        // Binary Search
+        while (left < right) {
+            // Calculate mid-point
+            int mid = left + (right - left) / 2;
+
+            // If mid-point element is greater than the last element
+            // of the array, then the minimum element must be in the
+            // right half of the array, so we update left pointer
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } 
+            // Otherwise, the minimum element must be in the left half
+            // of the array, so we update right pointer
+            else {
+                right = mid;
+            }
         }
-        
-        return nums[0];
+
+        // At the end of the while loop, left pointer points to the
+        // minimum element of the array, which is the answer
+        return nums[left];
     }
 }
 
@@ -31,6 +66,27 @@ public class Solution {
 /// Draft 
 
 /*
+
+  if ( nums[Middle]  > nums[Left])
+       Left = Middle + 1; 
+       
+       
+    - Here we were able to skip the middle by adding 1 ,
+    Because if the first element is less than it, 
+    Then the right side have a desending order that will contain
+    The Min number. 
+    
+    This condition just proved that our Middle is not the min number. 
+
+
+
+    else
+         Right = Middle;
+         
+    - This condition just keeps squeezing the side we choose,
+        To get out of the while and pick the far left in the
+        Final sub-array. 
+
 
 [3,4,5,1,2]
 
@@ -87,3 +143,42 @@ public class Solution {
 
 
 */
+
+
+
+// public class Solution {
+//     public int FindMin(int[] nums) {
+        
+        
+       
+//         var Left = 0; 
+//         var Right = nums.Length -1 ; 
+        
+        
+//         //This handles 2 cases:
+//         // 1- Array has 1 element `=`
+//         // 2- Array is completely sorted.
+//         if (nums[Left] <= nums[Right])
+//             return nums[Left];
+        
+        
+        
+        
+//         while ( Left <= Right)
+//         {
+//             var Middle = (Right - Left)/ 2  + Left ; 
+            
+            
+//             if ( nums[Middle]  > nums[Middle + 1])
+//                 return nums[Middle + 1]; // --> min number
+//             else if (nums[Right] < nums[Middle])
+//                 Left = Middle ; 
+//             else if (nums[Left] > nums[Middle])
+//                 Right = Middle; 
+//             else
+//                 return nums[0];
+//         }
+        
+//         return nums[0];
+//     }
+// }
